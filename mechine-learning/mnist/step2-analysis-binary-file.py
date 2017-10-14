501bfd7afd63510a81d52bb9f52313dc0cdca480
llet <504801864@qq.com>
2017-10-14 10:20:48 +0800

    分析原数据

--- mechine-learning/mnist/step2-analysis-binary-file.py
+++ mechine-learning/mnist/step2-analysis-binary-file.py
@@ -7,12 +7,14 @@
 参考: http://www.cnblogs.com/x1957/archive/2012/06/02/2531503.html
 模块		功能
 struct		对二进制数据打包和解包
-PIL		生成图片
+matplotlib	显示图片
 '''
 image='./data/train-images-idx3-ubyte'
 label='./data/train-labels-idx1-ubyte'
 
 import struct 
+import matplotlib.pyplot as plt
+import numpy as np 
 
 image=open(image,'rb')
 ibuf=image.read()
@@ -23,3 +25,15 @@ print struct.unpack_from('4B',struct.pack('i',2051),0),'pack("i",2051)'
 print struct.unpack_from('4B',struct.pack('>i',50855936),0),'pack(">i",50855936)'
 print struct.unpack_from('4B',struct.pack('>i',2051),0),'pack(">i",2051)'
 print '这就比较坑了,对于int默认使用小端规则进行pack和unpack'
+
+print 'integer1: ',struct.unpack_from('>i',ibuf,0)
+print 'integer2: ',struct.unpack_from('>i',ibuf,4)
+print 'integer3: ',struct.unpack_from('>i',ibuf,8)
+print 'integer4: ',struct.unpack_from('>i',ibuf,12)
+
+
+
+#再看看图片
+im=struct.unpack_from('784B',ibuf,16)
+plt.imshow(np.array(im).reshape(28,-1),cmap='gray')
+plt.show()
