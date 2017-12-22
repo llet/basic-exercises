70c9c799dd80436a7f4515b8d1932d599d7aaf1d
llet <504801864@qq.com>
2017-12-22 21:04:48 +0800

    python study

--- python/python基础.py
+++ python/python基础.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
 
-'''
-'''
+# 切片
+# 1
+s='Hello darkness, my old friend, I\'ve come to talk with you again ' 
+s=s[:-10:2]
 
-'''
-切片
-'''
-s = 'Hello darkness, my old friend, I\'ve come to talk with you again '
-s[:-10:2]
+# list生成
+# 1
+[x+y for x in 'ABC' for y in 'XYZ'] 
+# 2
+[x+'='+y for x,y in list({'X': 'A', 'Y': 'B', 'Z': 'C' }.items())] 
 
-'''
-生成器 generator
-'''
-
+# generator
+# 1
 g = (x * x for x in range(10))
-
-def f(i):
+# 2
+def f(i): 
   yield [1]
   x=[1,1]
   while len(x)<i:
@@ -24,19 +24,44 @@ def f(i):
     x.insert(0,1)
     x.appnd(1)
 
-'''
-迭代器 Iterable
-'''
-from collections import Iterable
+# Iterable&Iterator
+# 1
+from collections import Iterable,Iterator 
+isinstance((),Iterable) # True
+isinstance((),Iterator) # False 
+isinstance([],Iterator) # False
+isinstance(iter([]), Iterator) # True
+# 2
+x=iter([1,2,3,4]) 
+while True:
+  try:
+    print(next(x))
+  except StopIteration:
+    break
 
-isinstance((),Iterable)
+# map&reduce
+# 1
+def f(x): 
+  return x*x
+list(map(f,[1,2,3,4]))
+# 2
+from functools import reduce 
+def f(x,y):
+  return x*y
+reduce(f,[3,4,5,6,7])
+# 3
+def f(i): 
+  return reduce(lambda x,y:x+y**(-3),range(i+1))
 
-'''
-匿名函数
-'''
+# 匿名函数
+# 1
 list(map(lambda x: x*x ,range(20)))
-
+# 2
 list(filter(lambda x:x%2==0,range(20)))
-
+# 3
 def f(x, y):
   return lambda: x * x + y * y
+
+  
+
+  
