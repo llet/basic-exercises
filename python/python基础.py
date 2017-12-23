17b5a367dcaa3726ef5046b9ce6412957d3c29f7
llet <504801864@qq.com>
2017-12-23 18:20:31 +0800

    -

--- python/python基础.py
+++ python/python基础.py
@@ -8,18 +8,18 @@ s=s[:-10:2]
 [x+y for x in 'ABC' for y in 'XYZ'] 
 # list生成 2
 [x+'='+y for x,y in list({'X': 'A', 'Y': 'B', 'Z': 'C' }.items())] 
-
+	
 # generator 1
-g = (x * x for x in range(10))
+g=(x * x for x in range(10))
 # generator 2
 def f(i): 
-  yield [1]
-  x=[1,1]
-  while len(x)<i:
-    yield x
-    x = [x[i]+x[i+1] for i in range(len(x)-1)]
-    x.insert(0,1)
-    x.append(1)
+	yield [1]
+	x=[1,1]
+	while len(x)<i:
+		yield x
+		x = [x[i]+x[i+1] for i in range(len(x)-1)]
+		x.insert(0,1)
+		x.append(1)
 
 # Iterable & Iterator 1
 from collections import Iterable,Iterator 
@@ -31,41 +31,40 @@ isinstance(iter([]), Iterator) # True
 # Iterable & Iterator 2
 x=iter([1,2,3,4]) 
 while True:
-  try:
-    print(next(x))
-  except StopIteration:
-    break
+	try:
+		print(next(x))
+	except StopIteration:
+		break
 
 # map & reduce 1
 def f(x): 
-  return x*x
+	return x*x
 list(map(f,[1,2,3,4]))
 
 # map & reduce 2
 from functools import reduce 
 def f(x,y):
-  return x*y
+	return x*y
 reduce(f,[3,4,5,6,7])
 
 # map & reduce 3
 def f(i): 
-  return reduce(lambda x,y:x+y**(-3),range(i+1))
+	return reduce(lambda x,y:x+y**(-3),range(i+1))
 
 # filter 1
 list(filter(lambda x:x%2=1,range(20)))
 
-# filter 2
+# filter 2 
 def prime(max=100):
-  yield 2
-  gen=(i for i in range(3,max) if i%2==1 and i>1)
-  while True:
-    try:
-      n=next(gen)
-      yield n
-      gen=filter((lambda n:lambda x:x%n>0)(n),gen)
-    except StopIteration:
-      break
-
+	yield 2
+	gen=(i for i in range(3,max) if i%2==1 and i>1)
+	while True:
+		try:
+			n=next(gen)
+			yield n
+			gen=filter((lambda n:lambda x:x%n>0)(n),gen)
+		except StopIteration:
+			break
 
 # 匿名函数 1
 list(map(lambda x: x*x ,range(20)))
@@ -73,12 +72,9 @@ list(map(lambda x: x*x ,range(20)))
 list(filter(lambda x:x%2==0,range(20)))
 # 匿名函数 3
 def f(x, y):
-  return lambda: x * x + y * y
+	return lambda: x * x + y * y
 
-  
-for i,x in enumerate(primes()): # enumerate迭代遍历键值对(索引:素数)  
-    if i > 100000:  
-        break  
-    else:  
-        print(x)  
-  
+# __slots__
+class Student(object):
+		__slots__ = ('name', 'age') # __slots__ 限制实例的属性只能包含name,age
+
