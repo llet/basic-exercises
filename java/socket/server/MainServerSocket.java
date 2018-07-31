40ab2c329a06fd0d8e6185a518ae3e6daa8fe5fb
llet <504801864@qq.com>
2018-07-31 21:48:38 +0800

    x

--- java/socket/server/MainServerSocket.java
+++
@@ -1,7 +0,0 @@
-package main.server;
-
-public class MainServerSocket {
-	public static void main(String[] args) {
-		new ServerListener().start();
-	}
-}
--- yilianmengbi.md
+++
@@ -1,2 +0,0 @@
-java 编程中要用setter和getter来访问成员变量?
-
